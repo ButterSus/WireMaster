783d96ed2b6c88a48480e7c6ff9ebe2a6a56b488
buttersus <eduardkrivosapkin5@gmail.com>
2024-08-13 20:12:45 +0900

    rollback to stable commit

--- src/main/kotlin/com/buttersus/wireworks/client/keybinding/KeybindingHandler.kt
+++ src/main/kotlin/com/buttersus/wireworks/client/keybinding/KeybindingHandler.kt
@@ -1,20 +1,9 @@
 package com.buttersus.wireworks.client.keybinding
 
-import com.buttersus.wireworks.client.camera.OrthographicViewMode
-import com.buttersus.wireworks.extensions.toInt
 import net.minecraft.client.MinecraftClient
 
 object KeybindingHandler {
     fun onClientTick(client: MinecraftClient) {
-        if (Keybindings.TOGGLE_WIRE_DESIGNER.wasPressed()) {
-            OrthographicViewMode.toggle()
-        }
-
-        if (OrthographicViewMode.isActive) {
-            println("sus")
-            val forward = client.options.forwardKey.isPressed.toInt() - client.options.backKey.isPressed.toInt()
-            val sideways = client.options.rightKey.isPressed.toInt() - client.options.leftKey.isPressed.toInt()
-            OrthographicViewMode.movePlayer(forward, sideways)
-        }
+        if (Keybindings.TOGGLE_WIRE_DESIGNER.wasPressed()) println("Toggle Wire Designer")
     }
 }
